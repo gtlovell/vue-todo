f43bc0fafae5372980cfe76be28c2e9e35c1ff9e
gtlovell <garrett@garrettlovell.com>
2019-03-01 16:59:45 -0500

    first commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="list">
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem";
+
+export default {
+  name: "Todos",
+  components: {
+    TodoItem
+  },
+  props: ["todos"]
+};
+</script>
+
+<style scoped>
+.list {
+  width: 30%;
+  box-shadow: 0 8px 6px -6px rgb(181, 181, 181);
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    <h1>Todos</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+.header {
+  padding: 10px;
+  font-size: 20px;
+  color: coral;
+}
+</style>
+
